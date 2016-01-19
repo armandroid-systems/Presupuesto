9c7547c2ae497f168684a4a12742232d0dff1aea
armando.dominguez <armando.dominguez@globant.com>
2016-01-19 17:25:12 -0600

    Classes eliminated

--- app/src/main/java/com/armandroid/presupuesto/interactor/CurdBoussinesInteractorImpl.java
+++ app/src/main/java/com/armandroid/presupuesto/interactor/CurdBoussinesInteractorImpl.java
@@ -4,6 +4,8 @@ import android.content.Context;
 
 import com.armandroid.presupuesto.interfaces.BousinessCallback;
 import com.armandroid.presupuesto.model.AppDaoClass;
+import com.armandroid.presupuesto.model.Budget;
+import com.armandroid.presupuesto.model.Users;
 
 /**
  * Created by armando.dominguez on 18/01/2016.
@@ -40,9 +42,45 @@ public class CurdBoussinesInteractorImpl implements CurdBoussinesInteractor{
     @Override
     public void updateARecord(Object param, BousinessCallback bcb) {
         try{
-            bcb.onSucces(param);
+            bcb.onSucces(mDao.updateGeneric(param));
         }catch(Exception ex){
             bcb.onError(ex.getMessage());
+        }
+    }
+
+    @Override
+    public void getConfigData(String[] params, BousinessCallback bcb) {
+        try{
+            bcb.onSucces(mDao.getCatalogs(params));
+        }catch(Exception e){
+            bcb.onError(e.getMessage());
+        }
+    }
+
+    @Override
+    public void insertConfigData(String desc, float moneyEntry, Users user, BousinessCallback bcb) {
+        try{
+            bcb.onSucces(mDao.insertBundle(desc, moneyEntry,user));
+        }catch(Exception e){
+            bcb.onError(e.getMessage());
+        }
+    }
+
+    @Override
+    public void getBudgetDetail(int keyBudget, BousinessCallback bcb) {
+        try{
+            bcb.onSucces(mDao.getAllAboutBudget(keyBudget));
+        }catch(Exception e){
+            bcb.onError(e.getMessage());
+        }
+    }
+
+    @Override
+    public void getBudget(int keyUser, BousinessCallback bcb) {
+        try{
+            bcb.onSucces(mDao.getBudget(Budget.class, keyUser));
+        }catch(Exception e){
+            bcb.onError(e.getMessage());
         }
     }
 }
